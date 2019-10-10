1af1af88842e62e78837b7a74c5acbc842da1ee9
Skinde <migue.lama@yahoo.com>
2019-10-10 16:18:25 -0500

    Order Files

--- Source/main.cpp
+++ Source/main.cpp
@@ -4,7 +4,7 @@
 int main(){
 
 	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window *window = SDL_CreateWindow("owo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+	SDL_Window *window = SDL_CreateWindow("Hormiguero 2.0", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 	SDL_RenderPresent(renderer);
