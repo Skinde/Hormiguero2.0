8ffbfd14e96eaed47961824513a73a76a2c913df
Skinde <migue.lama@yahoo.com>
2019-10-24 13:55:07 -0500

    Funciones

--- Source/head.h
+++ Source/head.h
@@ -7,11 +7,31 @@ using namespace sf;
 
 class Funciones_De_Miguel
 {
+    private:
+    std::vector<sf::RectangleShape> array_de_rectangulos;
     public:
-    RectangleShape rectangulo(int x,int y,int tamano_x,int tamano_y)
+    void rectangulo(int x,int y,int tamano_x,int tamano_y)
     {
         RectangleShape rectangle(Vector2f(x,y));
         rectangle.setSize(Vector2f(tamano_x,tamano_y));
-        return rectangle;
+        array_de_rectangulos.push_back(rectangle);
     }
+    std::vector<RectangleShape> retornar_figuras()
+    {
+        return array_de_rectangulos;
+    }
+    void eliminar_rectangulo (RectangleShape& figurita)
+    {
+        for (int iterador = 0; iterador<array_de_rectangulos.size();iterador++)
+        {
+            if (array_de_rectangulos[iterador] == figurita)
+            {
+                array_de_rectangulos.erase(array_de_rectangulos.begin()+iterador);
+            }
+        }
+    }
+    
+    
+    
+
 };
--- Source/main.cpp
+++ Source/main.cpp
@@ -6,21 +6,36 @@
 
 int main()
 {
+    //Creacion de objetos y ventana
     Funciones_De_Miguel fm;
     RenderWindow Ventana(VideoMode(800, 600), "My window");
+    fm.rectangulo(10,10,200,200);
+
+
     while (Ventana.isOpen())
-    {
+    {   
+        // Manejador de eventos
         Event event;
-        while (Ventana.pollEvent(event))
+        if (Ventana.pollEvent(event))
         {
             if (event.type == Event::Closed)
                 Ventana.close();
         }
+        // Codigo
+
+
+
+        //Renderizador
         Ventana.clear(Color::Black);
-        Actualizar();
-        Renderizar();
-        Ventana.draw(fm.rectangulo(1,1,100,100));
+        if (fm.retornar_figuras().size() > 0)
+        {
+            for (auto figurin: fm.retornar_figuras())
+            {
+                Ventana.draw(figurin);
+            }
+        }
         Ventana.display();
+        
     }
     return 0;
 }
