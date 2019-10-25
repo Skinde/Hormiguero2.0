bd537c3d8f36f9fcc46145a30c5917373860f0f4
jean.huby <jean.paul@utec.edu.pe>
2019-10-25 09:51:28 -0500

    mas funciones, mas lineas, mas contribucion , mas notas

--- CDD/algoritmos.cpp
+++ CDD/algoritmos.cpp
@@ -7,14 +7,28 @@ double dot_product(std::vector<int> A,std::vector<int> B,std::vector<int> C,std:
 	//probando que se suba con mi usuario 2
 	double x,y;
 	x=A[0]*B[0]*C[0]*D[0];
-	y=A[1]*B[1]*C[0]*D[0];
+	y=A[1]*B[1]*C[1]*D[1];
 	return x+y;
+}
+std::vector <double> distance_vector(std::vector<int> A,std::vector<int> B){
+	std::vector <double> distance;
+	distance[0]=B[0]-A[0];
+	distance[1]=B[1]-A[1];
+	return distance;
+}
+
+double distance(std::vector <int> A, std::vector<int> B){
+	std::vector <double> temp=distance_vector(A,B);
+	return sqrt( temp[0]*temp[0]+ temp[1]*temp[1]);
+}
+double polarizacion(double A, double B){
+	return (A+B)/2;
 }
 
 //perling: 
 void perlin(int nfilas, int ncol){
-	//vector para contener las coordenadas x e y del mapa
-	int mapa [nfilas] [ncol];
+	//arry para contener las coordenadas valores en las coodenadas x e ydel mapa
+	double mapa [nfilas] [ncol];
 	//vector para almacenar los resultados del perling		
 	std::vector <std::vector<double>> gradiente (9,std::vector<double>(2));
 	for(int i=0;i<9;i++){
@@ -22,22 +36,33 @@ void perlin(int nfilas, int ncol){
 			gradiente[i][j]=randint(-1,1);
 		}
 	}
-	for()
-	// vector para los puntos aleatorios
-	int array;
-	int posx;
-	int posy;
-	for (int i=0;i<480000;i++)
-	{
-		posy = i-1/800;
-		posx = i;
-		while (posx > 800)
+	//modificando el mapa de z
+	for(int y=0; y<nfilas; y++)
+	{	
+		//sector 1//sector 2
+		//sector 3/sector 4
+		for(int x=0; x<ncol; x++)
 		{
-			posx -= 800;
-			std::cout << posx << " ";
-			std::cout <<  posy;
-			std::cout << std::endl;
+			if (y>0 && y<nfilas/2 && x>0 && x<ncol/2)
+			{
+				//Sector 1
+				mapa[x][y]=mapa[x][y]
+			}
+			if (y>0 && y<nfilas/2 && x>ncol/2 && x<ncol)
+			{
+				//Sector 2
+			}
+			if (y>nfilas/2 && y<nfilas && x>0 && x<ncol/2)
+			{
+				//Sector 3
+			}
+			if (y>nfilas/2 && y<nfilas && x>ncol/2 && x<ncol)
+			{
+				//Sector 4
+			}
+
 		}
 	}
+
 
 }
