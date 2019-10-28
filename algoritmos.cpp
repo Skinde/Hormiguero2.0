3e2c275da56f69800f839eece81453105d92617c
Dio12334 <50332452+Dio12334@users.noreply.github.com>
2019-10-27 19:29:55 -0500

    Update algoritmos.cpp

--- algoritmos.cpp
+++ algoritmos.cpp
@@ -4,10 +4,10 @@ float perlin(float x){
 
 	if(x <= 0.0)
 		x = 0.0;
-	else if(x<1)
+	else if(x<1.0)
 		x = 6*pow(x,5)-15*pow(x,4)+10*pow(x,3);
-	else if(x == 1)
-		x = 1;
+	else if(x == 1.0)
+		x = 1.0;
 	return x;
 
 }
