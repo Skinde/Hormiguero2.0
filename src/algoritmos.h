1c4984a4d344a432098ca27e56fff74a0781591d
jean.huby <jean.paul@utec.edu.pe>
2019-10-27 19:49:52 -0500

    declaration of smooth funciont fic

--- src/algoritmos.h
+++ src/algoritmos.h
@@ -15,7 +15,7 @@ class cosas_de_perlin
     float polarizacion(float  x,float  y,std::vector<float> A,std::vector<float> B,std::vector<float> C,std::vector<float> D);
     float**  perlin(int nfilas, int ncol);
     float distance(std::vector<float> A);
-    float  cosas_de_perlin::smooth (float  x)
+    float smooth (float  x);
 };
 #endif
 
